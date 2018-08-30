32fa00d7b33aca82408c7b1bdeb00cb557bc89df
Casey Zimmer <czimmer@ramapo.edu>
2018-08-30 22:53:24 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   # your code here
   
-  output_array = array.each do |element| (element * element) end
+  output_array = array.collect do |element| (element * element) end
   
 end
